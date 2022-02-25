89ea3b0350b268ee19066d5dac84eed68a2c34ab
abashir7866 <79613256+abashir7866@users.noreply.github.com>
2022-02-24 19:49:15 -0500

    Add files via upload

--- Ghost.py
+++ Ghost.py
@@ -2376,6 +2376,7 @@ Community Themes, run {Ghost.command_prefix}ctheme (theme name) to download the 
 {Ghost.command_prefix}suggest [suggestion] » Suggest something.
 {Ghost.command_prefix}privatemsg [message] » Send an encrypted message.
 {Ghost.command_prefix}privatemsgdecode [message] » Decode an encrypted message.
+{Ghost.command_prefix}vaporeon » Sends the Vaporeon copypasta
 {Ghost.command_prefix}blank » Send a blank message
 {Ghost.command_prefix}length [string] » Get the length of a string.
 {Ghost.command_prefix}chatbypass [text] » Bypass chat language restrictions.
@@ -2477,6 +2478,7 @@ Community Themes, run {Ghost.command_prefix}ctheme (theme name) to download the 
             if page == 1:
                 embed = discord.Embed(title=f"{__embedemoji__} **{__embedtitle__}** {__embedemoji__}", color=__embedcolour__, description=f"""
 `{Ghost.command_prefix}`**meme** » A random meme.
+`{Ghost.command_prefix}`**nighty** » Shows a Nighty user
 `{Ghost.command_prefix}`**cat** » A random cat image.
 `{Ghost.command_prefix}`**dog** » A random dog image.
 `{Ghost.command_prefix}`**shiba** » A random shiba image.
@@ -7291,6 +7293,13 @@ Address: {address}
         message = message_bytes.decode('ascii')
 
         await ctx.send(message)
+    @Ghost.command(name="nighty", description="nighty user = retard", usage="nighty")
+    async def nighty(ctx):
+        await ctx.send("https://media.discordapp.net/attachments/934555193838813214/946139744239239238/image0-16.gif")
+    
+    @Ghost.command(name="vaporeon", description="well uhh um you know", usage="vaporeon")
+    async def vaporeon(ctx):
+        await ctx.send("Hey guys, did you know that in terms of male human and female Pokémon breeding, Vaporeon is the most compatible Pokémon for humans? Not only are they in the field egg group, which is mostly comprised of mammals, Vaporeon are an average of 3”03’ tall and 63.9 pounds, this means they’re large enough to be able handle human dicks, and with their impressive Base Stats for HP and access to Acid Armor, you can be rough with one. Due to their mostly water based biology, there’s no doubt in my mind that an aroused Vaporeon would be incredibly wet, so wet that you could easily have sex with one for hours without getting sore. They can also learn the moves Attract, Baby-Doll Eyes, Captivate, Charm, and Tail Whip, along with not having fur to hide nipples, so it'd be incredibly easy for one to get you in the mood. With their abilities Water Absorb and Hydration, they can easily recover from fatigue with enough water. No other Pokémon comes close to this level of compatibility. Also, fun fact, if you pull out enough, you can make your Vaporeon turn white. Vaporeon is literally built for human dick. Ungodly defense stat+high HP pool+Acid Armor means it can take cock all day, all shapes and sizes and still come for more")
 
     @Ghost.command(name="encodebinary", description="Encode a message in binary.", usage="encodebinary [message]", aliases=["binaryencode", "binary"])
     async def encodebinary(ctx, *, message):
